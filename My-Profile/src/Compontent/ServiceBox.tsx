9165a89e8979c62f72f5e427ab605a547b4b0054
ugwucollins <ugwucollins027@gmail.com>
2024-08-15 20:37:33 +0100

    updated service.tsx
    just now

--- My-Profile/src/Compontent/ServiceBox.tsx
+++ My-Profile/src/Compontent/ServiceBox.tsx
@@ -7,6 +7,7 @@ const ServiceBox = ({
   paragraph2,
   links,
   links2,
+  links3,
 }: any) => {
   const [readmorebtn, setReadMorebtn] = useState(true);
   const [readmore, setReadMore] = useState(false);
@@ -27,23 +28,29 @@ const ServiceBox = ({
         <h3 className="Services-box-h3">{header3}</h3>
         <div className="Services-box-div word">
           {paragraph}
-          <a
-            style={{ fontSize: "14px", fontWeight: "bold" }}
-            href=" https://ugwucollins.github.io/Html-Css-and-JavaScrpit-projects/landening1/page.html"
-          >
-            {links}
-          </a>
 
           <span className={readmore ? "span-active" : "span-div"}>
             {/* <button className="Services-btn close" onClick={close}>
               <p>Read More</p>
             </button> */}
+            <a
+              style={{ fontSize: "14px", fontWeight: "bold" }}
+              href=" https://ugwucollins.github.io/Html-Css-and-JavaScrpit-projects/landening1/page.html"
+            >
+              {links}
+            </a>
             {paragraph2}
             <a
               style={{ fontSize: "14px", fontWeight: "bold" }}
               href=" https://ugwucollins.github.io/Html-Css-and-JavaScrpit-projects/landening1/page.html"
             >
               {links2}
+            </a>
+            <a
+              style={{ fontSize: "14px", fontWeight: "bold", marginTop: "4px" }}
+              href=" https://ugwucollins.github.io/Html-Css-and-JavaScrpit-projects/landening1/page.html"
+            >
+              {links3}
             </a>
           </span>
         </div>
--- My-Profile/src/Compontent/Services.tsx
+++ My-Profile/src/Compontent/Services.tsx
@@ -21,6 +21,7 @@ const Services = () => {
           paragraph2="I Am very detailed and use straightforward codes to ensure clean and easy maintenance, to mention but a few is the expertise in ensuring my projects often meets quality standards by using javaScript Frameworks like React.Js in building my Portfolio and also using Vercel in debuging(hosting) it and Using Only html and Tailwind css in building my Acme Rocket website fully mobile responsive.
           And many More like Next.Js, TypeScript e.tc"
           links2="https://rocket-tailwind.vercel.app/"
+          links3="TypeScript Link: https://next02-ten.vercel.app/"
         />
         {/* https://ugwucollins.github.io/Html-Css-and-JavaScrpit-projects/amazon-project/index.html */}
         {/* https://ugwucollins.github.io/Html-Css-and-JavaScrpit-projects/Birthday/ */}
@@ -31,25 +32,27 @@ const Services = () => {
           header3="Backend Developer"
           paragraph={`Am a backend developer  who have used Node.js and express.js in creating Apis that works well by using CRUD Api which helps you to create a post, Able to read and get it, update and Delete each post. And been able to get a post by it ID.
 
-            And also used html,css and javaScript to build the Amazon Economecs which was fully mobile responsive and also interactive you can check it out on :
+            And also used express,mongoose and mongodb to build Sign Up and Login Form being fully Vailated
             `}
-          links="https://ugwucollins.github.io/Html-Css-and-JavaScrpit-projects/amazon-project/index.html"
-          paragraph2="I Am very detailed and use straightforward codes to ensure clean and easy maintenance, to mention but a few is the expertise in ensuring my projects often meets quality standards by using javaScript Frameworks like React.Js in building my Portfolio and also using Vercel in debuging(hosting) it and Using Only html and Tailwind css in building my Acme Rocket website fully mobile responsive.
-          And many More like Next.Js, TypeScript e.tc"
-          links2="https://rocket-tailwind.vercel.app/"
+          // links="https://ugwucollins.github.io/Html-Css-and-JavaScrpit-projects/amazon-project/index.html"
+          paragraph2="I Am very detailed and use straightforward codes to ensure clean and easy maintenance, to mention but a few is the expertise in ensuring my projects often meets quality standards by using mongoose, mongodb  in building my Apis and also using firebase and mysql in building my database for easy mangement"
+          // links2="https://rocket-tailwind.vercel.app/"
         />
 
         <ServiceBox
-          // icon={<i className="bx bx-code-block"></i>}
           icon={<i className="bx bx-laptop"></i>}
           header3="Fullstack Developer"
-          paragraph=" PROFESSIONAL OBJECTIVE: To contribute in achieving organizational
-          goals through hard work and commitment to sound professional
-          practice in a diligent way while seeking for further improvement in
-          sound professional knowledge and career development."
-          paragraph2=" Passionate about learning and developing with a desire to apply for
+          paragraph={`Am a Fullstack developer  who have used Node.js,  express.js, and mongoose for the connection to mongodb  in creating My Blog_Post that is in form of a Compelcet Todo List App that help you to create a post, Update it or Edit it and also bein able to delete it by the Users ID with the use of JavaScript Framework => Ract.Js.
+            `}
+          //
+
+          // And also used express,mongoose and mongodb to build Sign Up and Login Form being fully Vailated
+          paragraph2={`And aslo used Node.js, express.Js , mongoose for the mongodb connection and React.Js that is coming from javaScript framework to build a sign Up and Sign Out Form.
+            Moreover am 
+            Passionate about learning and developing with a desire to apply for
           skills on a larger development team. Eager to tackle more complex
-          problems and continue to find ways to maximize user efficiency."
+          problems and continue to find ways to maximize user efficiency.
+            `}
         />
       </div>
     </div>
